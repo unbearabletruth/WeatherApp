8eb5163df706ed063525db282db85b395be15128
unbearabletruth <antaarty@gmail.com>
2023-05-01 17:11:27 +0700

    css changes

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 async function getData(city){
     try {
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=2f36dadd85054b5591e102613233004&q=${city}`);
@@ -12,15 +10,27 @@ async function getData(city){
     } 
 }
 
+function inputLocation(){
+    const searchInput = document.querySelector("input");
+    const city = searchInput.value;
+    getData(city);
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+    inputLocation();
+    e.preventDefault();
+})
+
 function filterData(data){
     const weatherInfo = {
         location: data.location.name,
         celsius: data.current.temp_c,
         fahrenheit: data.current.temp_f,
         condition: data.current.condition.text,
-        conditionIcon: data.current.condition.icon
+        conditionIcon: data.current.condition.icon,
+        wind: data.current.wind_kph
     }
-    console.log(weatherInfo.condition)
     render(weatherInfo);
     renderColor(weatherInfo.celsius);
 }
@@ -29,11 +39,13 @@ function render(info){
     const cityName = document.querySelector("#city");
     cityName.textContent = info.location;
     const temp = document.querySelector("#temperature");
-    temp.textContent = `${info.celsius}\u2103 / ${info.fahrenheit}\u2109`;
+    temp.textContent = `Temp: ${info.celsius}\u2103 / ${info.fahrenheit}\u2109`;
     const condition = document.querySelector("#conditionText");
     condition.textContent = info.condition;
     const conditionIcon = document.querySelector("#conditionImg");
     conditionIcon.src = info.conditionIcon;
+    const wind = document.querySelector("#wind");
+    wind.textContent = `Wind(km/h): ${info.wind}`;
 }
 
 function renderColor(celsius){
@@ -42,21 +54,11 @@ function renderColor(celsius){
         weatherCard.style.backgroundColor = "#fed7aa";
     } else if (celsius <= 15 && celsius > 5){
         weatherCard.style.backgroundColor = "#fff7ed";
-    } else if (celsius <=5 && celsius > -10){
+    } else if (celsius <= 5 && celsius > -10){
         weatherCard.style.backgroundColor = "white";
     } else {
         weatherCard.style.backgroundColor = "#bae6fd";
     }
 }
 
-function getLocation(){
-    const searchInput = document.querySelector("input");
-    const city = searchInput.value;
-    getData(city);
-}
-
-const button = document.querySelector("button");
-button.addEventListener("click", (e) => {
-    getLocation();
-    e.preventDefault();
-})
+getData("novosibirsk");
