94ee261a9f7b35e6fe80fcc9782e7602253acbf1
unbearabletruth <antaarty@gmail.com>
2023-05-01 15:58:40 +0700

    Added some css and more weather info

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const searchInput = document.querySelector("input");
-const errorInfo = document.querySelector("#errorInfo");
-const button = document.querySelector("button");
-button.addEventListener("click", (e) => {
-    getLocation();
-    e.preventDefault();
-})
+
 
 async function getData(city){
     try {
@@ -13,17 +7,56 @@ async function getData(city){
         filterData(data);
     } catch(error){
         console.log(error);
+        const errorInfo = document.querySelector("#errorInfo");
         errorInfo.textContent = "Make sure you entered a city name";
     } 
 }
 
 function filterData(data){
-    const tempCity = data.location.name;
-    const tempCel = data.current.temp_c;
-    console.log(`City: ${tempCity} temperature in Celsius: ${tempCel}`);
+    const weatherInfo = {
+        location: data.location.name,
+        celsius: data.current.temp_c,
+        fahrenheit: data.current.temp_f,
+        condition: data.current.condition.text,
+        conditionIcon: data.current.condition.icon
+    }
+    console.log(weatherInfo.condition)
+    render(weatherInfo);
+    renderColor(weatherInfo.celsius);
+}
+
+function render(info){
+    const cityName = document.querySelector("#city");
+    cityName.textContent = info.location;
+    const temp = document.querySelector("#temperature");
+    temp.textContent = `${info.celsius}\u2103 / ${info.fahrenheit}\u2109`;
+    const condition = document.querySelector("#conditionText");
+    condition.textContent = info.condition;
+    const conditionIcon = document.querySelector("#conditionImg");
+    conditionIcon.src = info.conditionIcon;
+}
+
+function renderColor(celsius){
+    const weatherCard = document.querySelector(".weatherInfo");
+    if (celsius > 15){
+        weatherCard.style.backgroundColor = "#fed7aa";
+    } else if (celsius <= 15 && celsius > 5){
+        weatherCard.style.backgroundColor = "#fff7ed";
+    } else if (celsius <=5 && celsius > -10){
+        weatherCard.style.backgroundColor = "white";
+    } else {
+        weatherCard.style.backgroundColor = "#bae6fd";
+    }
 }
 
 function getLocation(){
+    const searchInput = document.querySelector("input");
     const city = searchInput.value;
     getData(city);
 }
+
+const button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+    getLocation();
+    e.preventDefault();
+})
